6a65d67291905edba3babd2b3e03394b6cc5f597
MC <8493007+mcnaveen@users.noreply.github.com>
2026-09-06 07:30:03 +0530

    ✨ chore(build): update app version patch to 19

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 val appVersionMajor = 1
 val appVersionMinor = 9
-val appVersionPatch = 17
+val appVersionPatch = 19
 val appVersionCode = (appVersionMajor * 10000) + (appVersionMinor * 100) + appVersionPatch
 val appVersionName = "$appVersionMajor.$appVersionMinor.$appVersionPatch"
 
